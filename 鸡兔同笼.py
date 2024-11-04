889a3d3913263bc60e6ede8ec850687fdec2a085
wolf368 <2137315302@qq.com>
2024-11-04 09:40:12 +0800

    Add files via upload

---
+++ 鸡兔同笼.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder 编辑器
+
+这是一个临时脚本文件。
+"""
+head=35
+foot=94
+for i in range (1,head+1):
+    # range左闭右开,从1开始数到36，也可写作range（36），意味从0到36
+  if i*2+(35-i)*4==foot:
+        print(i,head-i)
+        
+     
+        
+ 
